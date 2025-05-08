fb7a51f5e8722a81a7e9637dcc27c267b5d69512
Niksi1911 <nikola.rizva1911@gmail.com>
2025-05-08 13:10:28 +0200

    registracija i login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,26 +1,44 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
- 
+  private accountService = inject(AccountService);
   http = inject(HttpClient);
   title = 'client';
-  users :any;
+  users: any;
 
   ngOnInit(): void {
-   this.http.get('https://localhost:5001/api/users').subscribe({
-    next: x=>this.users =x,
-    error: error => console.log(error),
-    complete: () => console.log("Completed")
-   })
+
+    this.getUsers();
+    this.setCurrentUser();
   }
-  
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: x => this.users = x,
+      error: error => console.log(error),
+      complete: () => console.log("Completed")
+    })
+  }
+
+
 }
