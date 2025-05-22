6731c1073a2648580e3bcd09b3d32d551b142dcb
Niksi1911 <nikola.rizva1911@gmail.com>
2025-05-23 00:20:55 +0200

    Dodan Tabs

--- client/src/app/memberdetail/memberdetail.component.ts
+++ client/src/app/memberdetail/memberdetail.component.ts
@@ -2,11 +2,13 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Members } from '../_models/members';
+import { Gallery, GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-memberdetail',
   standalone: true,
-  imports: [],
+  imports: [TabsModule,GalleryModule],
   templateUrl: './memberdetail.component.html',
   styleUrl: './memberdetail.component.css'
 })
@@ -19,13 +21,18 @@ export class MemberdetailComponent implements OnInit {
   private memberservice = inject(MembersService);
   private route = inject(ActivatedRoute);
   member?: Members;
+  images: GalleryItem[] =[];
 
   loadMember(){
     
     const username = this.route.snapshot.paramMap.get('username');
     if(!username)return;
     this.memberservice.getMember(username).subscribe({
-      next: member =>this.member = member
+      next: member =>{this.member = member;
+        member.photos.map(p=>{
+          this.images.push(new ImageItem({src: p.url,thumb:p.url}))
+        })
+      }
     })
   }
 
